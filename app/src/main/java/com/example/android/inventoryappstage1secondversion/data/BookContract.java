1fa8169ae7059c33535aebee7e38dad4fc290654
paintedlbird7 <rosaperezlarios@gmail.com>
2018-08-25 17:27:46 -0700

    Initial commit

---
+++ app/src/main/java/com/example/android/inventoryappstage1secondversion/data/BookContract.java
@@ -0,0 +1,4 @@
+package com.example.android.inventoryappstage1secondversion.data;
+
+public class BookContract {
+}
---
+++ app/src/main/java/com/example/android/inventoryappstage1secondversion/data/BookDBHelper.java
@@ -0,0 +1,3 @@
+package com.example.android.inventoryappstage1secondversion.data;
+
+
